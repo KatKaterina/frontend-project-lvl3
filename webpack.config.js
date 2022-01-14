dfe56da80fff58ff84f3cb52c2ec46c18335fa9b
Salnikova Katerina <katya-salnikova@mail.ru>
2022-01-14 12:26:04 +0300

    add webpack, bootstrap and config

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+};

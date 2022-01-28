0d7f8a901d4fb4a6391af5132bf4d196e18d48ed
Salnikova Katerina <katya-salnikova@mail.ru>
2022-01-28 15:41:41 +0300

    refactoring index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import handler from './handler.js';
 import init from './init.js';
 
 init();
-
-/*const agregation = () => {
-  const form = document.querySelector('form');
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    handler(form, e);
-    //forma.reser();
-    forma.focus();
-  });
-};
-
-agregation();
-*/

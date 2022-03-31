b4d410e9b741e6167bd10986fa3671f3b9b4cdef
KatKaterina <86184163+KatKaterina@users.noreply.github.com>
2022-03-31 20:33:43 +0300

    Update uploadRSS.js

--- src/uploadRSS.js
+++ src/uploadRSS.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default (url) => {
-  const pathRSS = `https://hexlet-allorigins.herokuapp.com/get?disableCache=true&url=${encodeURIComponent(url)}`;
+  const pathRSS = `https://hexlet-allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`;
   return axios.get(pathRSS);
 };

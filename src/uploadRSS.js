69c72d34f3ff37bf180abe728527105d4ca0ef77
Katerina Salnikova <katya-salnikova@mail.ru>
2022-03-31 22:18:14 +0300

    update uploader

--- src/uploadRSS.js
+++ src/uploadRSS.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
 export default (url) => {
-<<<<<<< HEAD
   const pathRSS = `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`;
-=======
-  const pathRSS = `https://hexlet-allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`;
->>>>>>> b4d410e9b741e6167bd10986fa3671f3b9b4cdef
   return axios.get(pathRSS);
 };
